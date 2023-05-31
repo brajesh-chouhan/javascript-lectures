b91b58651f8f5d5a7cc4309c909a796226c73db5
Brajesh chouhan <brajeshchouhan127@gmail.com>
2023-05-31 18:08:33 +0530

    first lect

---
+++ lecture-1/variable.js
@@ -0,0 +1,11 @@
+let result = 10;
+console.log(result);
+
+result = "hello";
+console.log(result);
+
+result = true;
+console.log(result);
+
+let isdelete = confirm("are you sure to delete this");
+console.log(isdelete);
